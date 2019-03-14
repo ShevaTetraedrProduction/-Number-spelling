190887ffaede3ce5fab27949956f032810fc90da
ShevaTetraedrProduction <37542753+ShevaTetraedrProduction@users.noreply.github.com>
2019-03-14 12:34:41 +0200

    Add files via upload

--- PropiseChusel/src/propisechusel/PropiseChusel.java
+++ PropiseChusel/src/propisechusel/PropiseChusel.java
@@ -2,6 +2,7 @@
 package propisechusel;
 
 import chuslo.Chuslo;
+import Count.Count;
 
 
 import java.util.Scanner;
@@ -13,6 +14,7 @@ public class PropiseChusel {
         Scanner in = new Scanner(System.in);
         Chuslo numb = new Chuslo();
         Nagoroda tar = new Nagoroda();
+        Count count = new Count();
 
         System.out.println(3 % 100);
 
@@ -20,22 +22,32 @@ public class PropiseChusel {
         System.out.println("Оберіть розділ для роботи");
         System.out.println("1---->перевід числа в пропис");
         System.out.println("2---->Нагорода");
-        System.out.println("3---->пропис тест");
-        System.out.println("4---->Формат числа");
+        System.out.println("3---->Кількість штук");
         byte unit;
 
         unit = in.nextByte();
         switch (unit) {
             case 1:
+                /*
+                240 000-wrong
+
+                 */
                 for (; ; ) {
-                    numb.setNumber(in.nextInt());
+                    numb.setNumber((int)in.nextDouble());
                     numb.allString();
                 }
 ////////////////////////////////////////////////////////////////////////////////               
             case 2:
                 for (int i = 0; i < 300; i++) {
-                    tar.setNumber(in.nextInt());
+                    tar.setNumber((int)in.nextDouble());
                     tar.count();
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < 300; i++) {
+                    count.setNumber((int)in.nextDouble());
+                    count.start();
                 }
                 break;
         }
